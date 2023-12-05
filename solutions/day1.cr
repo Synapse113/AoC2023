e83eabdebc2df3e8ef33b89b90f170e8762c64d0
Owen Nelson <onelson@Owens-MBP.localdomain>
2023-12-05 10:35:12 -0800

    Day 1 part 2 solved

--- solutions/day1.cr
+++ solutions/day1.cr
@@ -1,46 +1,21 @@
 file = File.read("../inputs/day1.txt")
-
-words = {"one" => "1", "two" => "2", "three" => "3", "four" => "4", "five" => "5", "six" => "6", "seven" => "7", "eight" => "8", "nine" => "9"}
-
 sum = 0
 
-def convert_word(w, wrds)
-  # puts w, w.size
+DIGITS = {"one" => "1", "two" => "2", "three" => "3", "four" => "4", "five" => "5", "six" => "6", "seven" => "7", "eight" => "8", "nine" => "9"}
 
-  if w == "one"
-    return "1"
-  elsif w == "two"
-    return "3"
-  elsif w == "three"
-    return "3"
-  elsif w == "four"
-    return "4"
-  elsif w == "five"
-    return "5"
-  elsif w == "six"
-    return "6"
-  elsif w == "seven"
-    return "7"
-  elsif w == "eight"
-    return "8"
-  elsif w == "nine"
-    return "9"
+def convert_to_num(word)
+  if word.size > 1
+    return DIGITS[word]
   end
-  return
 
-  # if w.size > 1
-  #   return wrds[w]
-  # end
-
-  # return w
+  return word
 end
 
 file.each_line do |l|
-  word_matches = l.scan(/#{words.keys.join("|")}|[0-9]/m)
+  pattern = DIGITS.keys.join("|") + "|" + DIGITS.values.join("|")
+  matches = l.scan(/(?=(#{pattern}))/m)
 
-  num = (convert_word(word_matches[0][0], words) + convert_word(word_matches[-1][0], words)).to_i
-
-  sum += num
+  sum += (convert_to_num(matches[0][1]) + convert_to_num(matches[-1][1])).to_i
 end
 
 p! sum
