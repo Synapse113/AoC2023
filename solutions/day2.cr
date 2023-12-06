825bc8fae53f9fd95a0ef1876bbacd4ab5af8dd9
Synapse113 <owen.isaiah113@gmail.com>
2023-12-05 19:16:16 -0800

    day 2 part 1,  gross version

--- solutions/day2.cr
+++ solutions/day2.cr
@@ -1,13 +1,39 @@
 file = File.read("../inputs/day2.txt")
 
-def parse_line(l)
-	# Game ([0-9]): ([0-9] \w+), ([0-9] \w+), ([0-9] \w+)
-	# ([0-9]+ \w+)
-	iterations = l.split(';')
+MAX_RED   = 12
+MAX_GREEN = 13
+MAX_BLUE  = 14
 
-	puts iterations
+IDS = [] of Int32
+
+def parse_line(l)
+  rounds = l.gsub(',', "").split(/; |: /)
+  game_id = rounds[0].scan(/[0-9]+/)[0][0]
+  rounds.shift
+
+  colors = {"red" => 0, "green" => 0, "blue" => 0}
+
+  rounds.each do |r|
+    cubes = r.split(/ /)
+
+    cubes.each_with_index do |c, i|
+      if c.size > 2
+        if cubes[i - 1].to_i > colors[c]
+          colors[c] = cubes[i - 1].to_i
+        end
+      end
+    end
+  end
+
+  if colors["red"] <= MAX_RED && colors["blue"] <= MAX_BLUE && colors["green"] <= MAX_GREEN
+    IDS << game_id.to_i
+  end
 end
 
 file.each_line do |l|
-	parsed = parse_line(l);
+  parsed = parse_line(l)
 end
+
+sum = IDS.sum
+
+p! sum
